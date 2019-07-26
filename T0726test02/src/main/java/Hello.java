35e8eae8e221172813aaea770293869d80670b22
Elsamun <53335810+Elsamun@users.noreply.github.com>
2019-07-26 18:44:20 +0800

    Update Hello.java

--- T0726test02/src/main/java/Hello.java
+++ T0726test02/src/main/java/Hello.java
@@ -7,5 +7,19 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("哈哈哈哈哈哈");
+        
+        System.out.println("哈哈哈哈哈哈");
+        
+        
+        System.out.println("哈哈哈哈哈哈");
+        
+        
+        
+        System.out.println("哈哈哈哈哈哈");
+        
+        
+        
+        
+        System.out.println("哈哈哈哈哈哈");
     }
 }
