d25e664bb907070957c283bd3674132076acc654
elsam <675251078@qq.com>
2019-07-26 18:30:33 +0800

    test03

--- T0726test01/src/main/java/Hellow.java
+++ T0726test01/src/main/java/Hellow.java
@@ -7,5 +7,8 @@
 public class Hellow {
     public static void main(String[] args) {
         System.out.println("哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈");
     }
 }
