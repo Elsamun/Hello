11fc38dad6ebad550e8d845efcec9e43ec5c594f
elsam <675251078@qq.com>
2019-07-26 18:18:37 +0800

    test02

---
+++ T0726test01/src/main/java/Hellow.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @Author: elsam
+ * @Version v 1.1
+ * @Date: Created in 18:17 2019/7/26.
+ */
+public class Hellow {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈哈哈");
+    }
+}
